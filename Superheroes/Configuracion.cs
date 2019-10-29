70eabf1059a09b8303d95486627706ffd509023a
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-10-29 12:54:45 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/Configuracion.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+class Configuracion : INotifyPropertyChanged
+{
+    public string AjusteImagen { get; set; }
+    public string Fondo { get; set; }
+
+    public Configuracion()
+    {
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
+
